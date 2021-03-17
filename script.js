c6835149eefe56072af82c14268a6f4402618066
Viktor Prilepin <prilepinva@gmail.com>
2021-03-17 20:02:13 +0700

    first dommit

---
+++ script.js
@@ -0,0 +1,20 @@
+function name1(element) {
+    return element
+}
+
+
+let qwe = name1({})
+
+
+let node = document.getElementById("asd")
+
+document.getElementsByClassName("sdl;fk;sld")
+
+console.log("sdlfklsdjfl")
+
+console.log('');
+
+console.error(";askd;ak");
+
+
+
